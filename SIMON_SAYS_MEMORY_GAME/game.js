3baee3f41f2ad16cfc8629b1bc154db75584f3ab
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-07-20 11:04:35 -0400

    trying to figure this out

--- SIMON_SAYS_MEMORY_GAME/game.js
+++ SIMON_SAYS_MEMORY_GAME/game.js
@@ -1,4 +1,12 @@
-// First I have to identify the human and computer
-let player = [];
-let computer = []
 
+// use query to connect things from html to js?
+const red = document.querySelector('.square');
+const green = document.querySelector('.square1');
+const pink = document.querySelector('.square3');
+const purple = document.querySelector('.square2');
+const start = document.querySelector('.button')
+
+//should be levels to start things off, and a computer 
+function START(){
+
+}
