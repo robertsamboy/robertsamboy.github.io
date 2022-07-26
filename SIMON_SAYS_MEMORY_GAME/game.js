d5788b9d8b76e835b7614eaaa88f3a30aca42626
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-07-26 14:36:12 -0400

    struggling

--- SIMON_SAYS_MEMORY_GAME/game.js
+++ SIMON_SAYS_MEMORY_GAME/game.js
@@ -1,17 +1,11 @@
+let player = [];
+let computer = [];
+let level = 0;
 
-// use query to connect things from html to js?
-const red = document.querySelector(".square");
-const green = document.querySelector(".square1");
-const pink = document.querySelector(".square3");
-const purple = document.querySelector(".square2");
-const start = document.querySelector(".button")
-
-//should be levels to start things off, and a computer  
-
-
-
-
-
+const red = document.querySelector(".square-red");
+const pruple = d;
+const green = v;
+const pink = 
 
 
 
